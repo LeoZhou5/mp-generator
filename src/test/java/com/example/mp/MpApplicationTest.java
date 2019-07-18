afae2301327fffc427705d1c711d6b4854c5cfdc
Leo <824095775@qq.com>
2019-07-18 16:42:30 +0800

    add test

---
+++ src/test/java/com/example/mp/MpApplicationTest.java
@@ -0,0 +1,20 @@
+package com.example.mp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Leo
+ * @date 2019/7/18 16:19
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MpApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+    
+}
